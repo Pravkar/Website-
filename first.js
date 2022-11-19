a6b17393a95bbcd08956ccceaa5a0a5643eca15f
Pravakar Adhikari <116089042+Pravkar@users.noreply.github.com>
2022-11-20 02:12:06 +0545

    Add files via upload

---
+++ first.js
@@ -0,0 +1,20 @@
+function Red()
+{
+    document.body.style.background="red";
+}
+
+function yellow()
+{
+    document.body.style.background="yellow";
+}
+
+
+function Green()
+{
+    document.body.style.background="green";
+}
+
+function Blue()
+{
+    document.body.style.background="blue";
+}
